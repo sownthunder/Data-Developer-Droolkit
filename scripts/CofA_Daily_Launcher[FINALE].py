e1d877bafd2faea9d84bb356c7259ec8152ee346
sownthunder <dbates117@gmail.com>
2019-08-08 16:52:27 -0400

    anothaaaa one

---
+++ scripts/CofA_Daily_Launcher[FINALE].py
@@ -0,0 +1,18 @@
+"""
+Created on August 08, 2019
+
+transfer from/finalization of:
+"CofA_Daily_Launhcer_[Finale].ipynb"
+
+Each day scans within:
+F:/APPS/CofA/
+AND ANY NEW FILE THAT COMES IN:
+
+YOU KNOW THE REST
+
+"""
+
+# IMPORT THE GOODS
+import os, sys, time
+from time import sleep
+from pathlib import Path
